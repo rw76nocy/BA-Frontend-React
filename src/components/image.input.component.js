f7a232017b0c38f558dc8c431f6aaaac18fb28e0
Rico Warnke <warnke.rico@gmx.net>
2022-03-17 10:56:35 +0100

    refactor image input

--- src/components/image.input.component.js
+++ src/components/image.input.component.js
@@ -1,15 +1,22 @@
 import React, {useState} from "react";
 
 import '../style/input.component.css';
+import Trash from "../icons/trash.svg";
 
 export default function ImageInput({title}) {
 
     const [image, setImage] = useState(null);
+    const [inputkey, setInputKey] = useState();
 
     const onImageChange = (e) => {
         if (e.target.files && e.target.files[0]) {
             setImage(URL.createObjectURL(e.target.files[0]));
         }
+    }
+
+    const onDeleteClick = () => {
+        setImage(null);
+        setInputKey(Date.now);
     }
 
     return(
@@ -21,7 +28,13 @@ export default function ImageInput({title}) {
                         <img className="image-element" name="image-element" id="image-element" src={image} alt="Hier kann ein Foto eingefügt werden"/>
                     </span>
                     <span className="image-row">
-                        <input onChange={onImageChange} className="input-input" name="image" id="image" type="file"/>
+                        <input onChange={onImageChange} className="image-input" key={inputkey} name="image" id="image" type="file"/>
+                        <input className="image-delete"
+                               type="image"
+                               src={Trash}
+                               alt="löschen"
+                               onClick={onDeleteClick}
+                        />
                     </span>
                 </div>
             </div>
