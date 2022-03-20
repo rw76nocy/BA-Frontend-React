753f24dd5aead8da9119a77a57018dc1a78b5b0d
Rico Warnke <warnke.rico@gmx.net>
2022-03-20 18:37:20 +0100

    refactor text component input

--- src/components/children.component.js
+++ src/components/children.component.js
@@ -17,11 +17,19 @@ import '../style/children.component.css';
 export default function Children() {
 
     const [image, setImage] = useState("");
+    const [reason, setReason] = useState("");
     const [guardian, setGuardian] = useState({});
     const [asd, setAsd] = useState({});
+    const [care, setCare] = useState("");
+    const [visit, setVisit] = useState("");
+    const [diseases, setDiseases] = useState("");
 
     const getInputAsImage = (input) => {
         setImage(input);
+    }
+
+    const getInputAsReason = (input) => {
+        setReason(input);
     }
 
     const getInputAsGuardian = (input) => {
@@ -32,10 +40,26 @@ export default function Children() {
         setAsd(input);
     }
 
+    const getInputAsCare = (input) => {
+        setCare(input);
+    }
+
+    const getInputAsVisit = (input) => {
+        setVisit(input);
+    }
+
+    const getInputAsDiseases = (input) => {
+        setDiseases(input);
+    }
+
     const printData = () => {
         console.log("Image: "+JSON.stringify(image));
+        console.log("Reason: "+JSON.stringify(reason));
         console.log("Guardian: "+JSON.stringify(guardian));
         console.log("Asd: "+JSON.stringify(asd));
+        console.log("Care: "+JSON.stringify(care));
+        console.log("Visit: "+JSON.stringify(visit));
+        console.log("Diseases: "+JSON.stringify(diseases));
     }
 
     return(
@@ -43,16 +67,16 @@ export default function Children() {
             <div className="children-components">
                 <ImageInput title="Foto" callback={getInputAsImage}/>
                 <PersonalDataInput title="Persönliche Daten"/>
-                <TextInput title="Grund der Aufnahme"/>
+                <TextInput title="Grund der Aufnahme" callback={getInputAsReason}/>
                 <GuardianInput title="Vormund/Pfleger" callback={getInputAsGuardian}/>
                 <AsdInput title="Allgemeiner sozialer Dienst" callback={getInputAsAsd}/>
                 <PersonInput title="Mutter"/>
                 <PersonInput title="Vater"/>
-                <TextInput title="Regelung Sorgerecht"/>
-                <TextInput title="Besuch / Umgang / Kontakt"/>
+                <TextInput title="Regelung Sorgerecht" callback={getInputAsCare}/>
+                <TextInput title="Besuch / Umgang / Kontakt" callback={getInputAsVisit}/>
                 <ChildDoctorInput title="Kinderarzt"/>
                 <DayCareInput title="Kita"/>
-                <TextInput title="Erkrankungen / Medikamente"/>
+                <TextInput title="Erkrankungen / Medikamente" callback={getInputAsDiseases}/>
                 <HealthInsuranceInput title="Krankenkasse"/>
                 <FoodSupplierInput title="Essensanbieter"/>
                 <InstitutionInput title="Fahrdienst"/>
--- src/components/text.input.component.js
+++ src/components/text.input.component.js
@@ -2,14 +2,21 @@ import React, {useState} from "react";
 
 import '../style/input.component.css';
 
-export default function TextInput({title}) {
+export default function TextInput({title, callback}) {
+
+    const [input, setInput] = useState("");
+
+    const onChangeInput = (e) => {
+        setInput(e.target.value);
+        callback(e.target.value);
+    }
 
     return(
         <div className="input-border">
             <div className="input-container">
                 <h3><u>{title}</u></h3>
                 <div className="input-create-container">
-                    <textarea className="textarea-input" name="text" id="text" cols="70" rows="11" wrap="soft"/>
+                    <textarea onChange={onChangeInput} className="textarea-input" value={input} name="text" id="text" cols="70" rows="11" wrap="soft"/>
                 </div>
             </div>
         </div>
